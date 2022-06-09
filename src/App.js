c95a4136f00961764d8b0b69e134971b8c6f3310
nancy <nancypurwar5@gmail.com>
2022-06-09 18:28:09 +0530

    added LanguageSelect.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
+import LanguageSelector from './components/LanguageSelector';
 import UserCreate from './components/UserCreate';
 import ColorContext from './contexts/ColorContext';
-import LanguageContext from './contexts/LanguageContext';
+import { LanguageStore } from './contexts/LanguageContext';
 
 export default class App extends Component {
-  state = {
-    language: 'english'
-  };
-  onLanguageChange = language => {
-    this.setState({ language: language });
-  }
+
   render() {
     return (
       <div className='ui container'>
-        <div>
-          <h3>Select a Language:</h3>
-          <i className='flag us' onClick={() => this.onLanguageChange('english')} />
-          <i className='flag nl' onClick={() => this.onLanguageChange('dutch')} />
-        </div>
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     )
   }
